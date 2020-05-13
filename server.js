6d76022dade3eceff39d9ffb0f8e1448a1fc9e06
Pascal Wegner <pascal.wegner@fastleansmart.com>
2020-05-13 14:34:44 +0200

    Change wrong file name

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 app.use(express.static("public"));
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "malicious.html"));
 });
 app.listen(8081, () => {
   console.log("Listening on port 8081...");
